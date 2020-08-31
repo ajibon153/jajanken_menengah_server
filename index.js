6a2d868727802bbfc4bfea8da4f5bb644223f869
ajibon <ajicwgkng@gmail.com>
2020-08-31 21:27:15 +0700

    updt senin

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
-users = [];
+// let io = require("socket.io")(server, {
+//   // path: "/",
+//   serveClient: true,
+//   transports: ["websocket", "polling"],
+//   // below are engine.IO options
+//   pingInterval: 10000,
+//   pingTimeout: 5000,
+//   cookie: true,
+// }).listen(server);
+// io.listen(server);
+let users = [];
 connections = [];
 choices = [];
+temp_choices = [];
 var score = 0;
 const router = require("./router");
 
@@ -22,153 +33,213 @@ app.use(router);
 // if (lasttime - Date.now() < 1000) {
 //   return null;
 // }
+// setInterval(function () {
+// io.set("transports", ["websocket"]);
+// setInterval(function () {
 io.sockets.on("connection", function (socket) {
+  // setInterval(function () {
   connections.push(socket);
   console.log("Connected: %s sockets connected", connections.length);
+  // }, 2000);
 
-  socket.on("disconnect", function (data) {
-    users.splice(users.indexOf(socket.username), 1);
-    updateUsernames();
-    connections.splice(connections.indexOf(socket), 1);
-    io.emit("disconnected", socket.username);
-    console.log("Disconnected: %s sockets connected", connections.length);
-  });
+  // socket.on("disconnect", function (data) {
+  //   users.splice(users.indexOf(socket.username), 1);
+  //   updateUsernames();
+  //   connections.splice(connections.indexOf(socket), 1);
+  //   io.emit("disconnected", socket.username);
+  //   // users = [];
+  //   console.log("Disconnected: %s sockets connected", connections.length);
+  // });
 
-  socket.on("send message", function (data) {
-    io.sockets.emit("new message", { msg: data, user: socket.username });
-  });
+  socket.on("send message", function (data, callback) {
+    // const result = JSON.parse(message.utf8Data);
+    const { method } = data;
 
-  socket.on("add user", function (data, callback) {
-    socket.username = data;
+    // io.sockets.emit("new message", { msg: data, user: socket.username });
+    if (method === "add user") {
+      socket.username = data;
+      if (users.indexOf(socket.username) > -1) {
+        callback(false);
+      } else {
+        let payload;
+        if (Object.keys(users).length < 1) {
+          payload = {
+            name: data.name,
+            player: "one",
+            score: data.score,
+          };
+        } else {
+          payload = {
+            name: data.name,
+            player: "two",
+            score: data.score,
+          };
+        }
+        users.push(payload);
+        // users.concat(socket.username);
+        updateUsernames();
+        callback(payload);
 
-    if (users.indexOf(socket.username) > -1) {
-      callback(false);
-    } else {
-      users.push(socket.username);
-      updateUsernames();
-      callback(true);
+        if (Object.keys(users).length == 2) {
+          io.emit("connected", socket.username);
+          io.emit("game start");
+        }
+      }
+      console.log("after add ", users);
 
-      if (Object.keys(users).length == 2) {
-        io.emit("connected", socket.username);
-        io.emit("game start");
+      // });
+    }
+    if (method === "player choice") {
+      console.log("start choice", data);
+      const { name, choice, player } = data;
+      temp_choices.push(data);
+      // console.log(temp_choices);
+      // Array.prototype.move = function (from, to) {
+      //   this.splice(to, 0, this.splice(from, 1)[0]);
+      // };
+      const swapPositions = (array, a, b) => {
+        return ([array[a], array[b]] = [array[b], array[a]]);
+      };
+      console.log(temp_choices);
+
+      if (temp_choices.length == 2) {
+        if (temp_choices[0]["player"] == "two") {
+          let ar = swapPositions(temp_choices, 0, 1);
+          choices.push(ar);
+        } else if (temp_choices[1]["player"] == "one") {
+          let ar = swapPositions(temp_choices, 0, 1);
+          choices.push(ar);
+        } else {
+          choices.push(temp_choices);
+        }
+        console.log("all choices", choices);
+        console.log("==============================================");
+        console.log("%s chose %s.", name, choice);
+        console.log('choices[0][0]["choice"]', choices[0][0]["choice"]);
+        console.log('choices[0][1]["choice"]', choices[0][1]["choice"]);
+        switch (choices[0][0]["choice"]) {
+          case "batu":
+            switch (choices[0][1]["choice"]) {
+              case "batu":
+                io.emit("tie", choices);
+                callback(choices);
+                console.log("set", choices);
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "kertas":
+                callback(choices);
+                io.emit("player 2 win", choices);
+                console.log("set", choices);
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "gunting":
+                callback(choices);
+                io.emit("player 1 win", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              default:
+                break;
+            }
+            break;
+
+          case "kertas":
+            switch (choices[0][1]["choice"]) {
+              case "batu":
+                callback(choices);
+                io.emit("player 1 win", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "kertas":
+                callback(choices);
+                io.emit("tie", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "gunting":
+                callback(choices);
+                io.emit("player 2 win", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              default:
+                break;
+            }
+            break;
+
+          case "gunting":
+            switch (choices[0][1]["choice"]) {
+              case "batu":
+                callback(choices);
+                io.emit("player 2 win", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "kertas":
+                callback(choices);
+                io.emit("player 1 win", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              case "gunting":
+                callback(choices);
+                io.emit("tie", choices);
+                console.log("set", choices);
+
+                choices = [];
+                temp_choices = Array();
+                break;
+
+              default:
+                break;
+            }
+            break;
+
+          default:
+            break;
+        }
+
+        choices = [];
       }
     }
   });
 
-  socket.on("player choice", function (username, choice) {
-    choices.push({ user: username, choice: choice });
-    console.log("%s chose %s.", username, choice);
-
-    if (choices.length == 2) {
-      //   console.log("[socket.io] Both players have made choices.");
-      //   console.log("masuk ke kondisi");
-      //   console.log("choices[0]", choices[0]);
-      //   console.log('choices[0]["choice"]', choices[0]["choice"]);
-      //   console.log("choices[1]", choices[1]);
-      //   console.log('choices[1]["choice"]', choices[1]["choice"]);
-      //   if (choices[0]["choice"] === "batu") {
-      //       if (choices[1]["choice"] === "batu") {
-      //           io.emit("tie", choices);
-      //           console.log("tie");
-      //       } else if (choices[1]["choice"] === "kertas") {
-      //           console.log("player 2 win");
-      //           io.emit("player 2 win", choices);
-      //       } else if (choices[1]["choice"] === "gunting") {
-      //           console.log("player 1 win");
-      //           io.emit("player 1 win", choices);
-      //     }
-      //   } else if (choices[0]["choice"] === "kertas") {
-      //     if (choices[1]["choice"] === "batu") {
-      //     } else if (choices[1]["choice"] === "kertas") {
-      //     } else if (choices[1]["choice"] === "gunting") {
-      //     }
-      //   } else if (choices[0]["choice"] === "gunting") {
-      //     if (choices[1]["choice"] === "batu") {
-      //       //
-      //     } else if (choices[1]["choice"] === "kertas") {
-      //       //
-      //     } else if (choices[1]["choice"] === "gunting") {
-      //       //
-      //     }
-      //   }
-      switch (choices[0]["choice"]) {
-        case "batu":
-          switch (choices[1]["choice"]) {
-            case "batu":
-              io.emit("tie", choices);
-              console.log("tie");
-              break;
-
-            case "kertas":
-              console.log("player 1win");
-              io.emit("player 2 win", choices);
-              break;
-
-            case "gunting":
-              console.log("player 2win");
-              io.emit("player 1 win", choices);
-              break;
-
-            default:
-              break;
-          }
-          break;
-
-        case "kertas":
-          switch (choices[1]["choice"]) {
-            case "batu":
-              console.log("p1win");
-              io.emit("player 1 win", choices);
-              break;
-
-            case "kertas":
-              console.log("tie");
-              io.emit("tie", choices);
-              break;
-
-            case "gunting":
-              console.log("p2win");
-              io.emit("player 2 win", choices);
-              break;
-
-            default:
-              break;
-          }
-          break;
-
-        case "gunting":
-          switch (choices[1]["choice"]) {
-            case "batu":
-              console.log("p2in");
-              io.emit("player 2 win", choices);
-              break;
-
-            case "kertas":
-              console.log("p1win");
-              io.emit("player 1 win", choices);
-              break;
-
-            case "gunting":
-              console.log("tie");
-              io.emit("tie", choices);
-              break;
-
-            default:
-              break;
-          }
-          break;
-
-        default:
-          break;
-      }
-
-      choices = [];
-    }
-  });
+  // socket.on("player choice", function (username, choice) {
+  // });
   socket.on("uncaughtException", function (err) {
     console.info(util.inspect(err, { colors: true }));
   });
-  function updateUsernames() {
-    io.sockets.emit("get user", users);
-  }
 });
+// }, 1000);
+function updateUsernames() {
+  // console.log("update", users);
+  // setInterval(function () {
+  io.sockets.emit("get user", users);
+  console.log("update users", users);
+  // }, 1000);
+}
+// }, 3000);
+//
